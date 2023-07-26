e9d7e17225f50d7d51e0b14e80a3ea62dda3fceb
VincentDirac <1350709199@qq.com>
2023-07-26 11:52:19 +0800

    添加一个bgm文件名与音乐名对照。

--- transBFA.py
+++ transBFA.py
@@ -1,8 +1,10 @@
 f = open("BgmForAll.ini", "r+", encoding="utf-8")
 h = open("BGMForAll+.ini", "w+", encoding="utf-8")
+bgmList = []
 for bgmDataLines in f.readlines():
     if "BGM = " in bgmDataLines:
         bgmDataLines = bgmDataLines.split(" ")
+        bgmList.append([bgmDataLines[2]])
         if "TH128_08" in bgmDataLines[2]:
             bgmDataLines[2] = "プレイヤーズスコア,"
         g = open("./data/musiccmt.txt", "r+", encoding="utf-8")
@@ -12,9 +14,17 @@ for bgmDataLines in f.readlines():
             if "♪" in bgmNameLines:
                 if bgmFileName.upper() in bgmDataLines[2]:
                     bgmName = bgmNameLines.lstrip("♪").rstrip("\n")
+                    bgmList[-1].append(bgmName)
                     bgmDataLines[2] = bgmName + ","
                     break
         bgmDataLines = (" ").join(bgmDataLines)
         h.write(bgmDataLines)
     else:
         h.write(bgmDataLines)
+bgmListFile = open("bgmList.txt", "w+", encoding="utf-8")
+for i in bgmList:
+    bgmListFile.write(i[0] + ", " + i[1] + "\n")
+f.close()
+g.close()
+h.close()
+bgmListFile.close()
